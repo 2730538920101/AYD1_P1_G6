b3490c0e6959f44626aa5e6136287dbcb5480685
2730538920101 <2730538920101@ingenieria.usac.edu.gt>
2023-02-15 18:30:13 -0600

    [ADD] DDL para crear tablas en la base de datos

---
+++ Database/contactos_init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS CONTACTO
+ (
+ IdContacto int  primary key not null auto_increment,
+ Nombre varchar (25),
+ Apellido Varchar (25),
+ Telefono Varchar (12),
+ Correo varchar (50)
+ );
+
+CREATE TABLE IF NOT EXISTS FAVORITO
+(
+IdFavorito int not null,
+FOREIGN KEY(IdFavorito) REFERENCES CONTACTO(IdContacto)
+ON DELETE CASCADE ON UPDATE CASCADE
+);

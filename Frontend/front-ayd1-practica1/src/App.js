9e929b61321c3416c3e3c6c37f9fa7e32a475c1e
Daniel Chicas <chicasdaniel53@gmail.com>
2023-02-16 15:49:01 -0600

    [ADD] componente navbar

--- Frontend/front-ayd1-practica1/src/App.js
+++ Frontend/front-ayd1-practica1/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './componentes/navegacion/Navbar';
+import AcercaDe from './componentes/AcercaDe';
+import Contacto from './componentes/Contacto';
+import Favoritos from './componentes/Favoritos';
+import Inicio from './componentes/Inicio';
+import NuevoContacto from './componentes/NuevoContacto';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        
+        <Routes>
+          <Route path={'/Inicio'} element={<Inicio/>} />
+          <Route path={'/Contacto'} element={<Contacto/>} />
+          <Route path={'/Favoritos'} element={<Favoritos/>} />
+          <Route path={'/Nuevo'} element={<NuevoContacto/>} />
+          <Route path={'/Acerca'} element={<AcercaDe/>} />
+        </Routes>
+
+      </Router> 
     </div>
   );
 }
